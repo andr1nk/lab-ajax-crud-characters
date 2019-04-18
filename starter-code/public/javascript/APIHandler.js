1043c0a2febf3760d1845310287cb55810022bd1
andr1nk <andre@avertu.ch>
2019-04-18 23:00:32 +0200

    all done except button background changes

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -10,44 +10,35 @@ class APIHandler {
       return data
     });
   }
-  
+
   getOneRegister() {
     document.querySelector('.characters-container').innerHTML = "";
     return axios.get(`${this.BASE_URL}/characters/${document.getElementById("fetch-one-input").value}`).then(response => {
       const { data } = response;
-      console.log(data)
       return data;
     });
-    // let oneId = document.getElementById("fetch-one-input").value
-    // axios.get(`${this.BASE_URL}/characters/${oneId}`)
-    //   .then(response => {
-    //     const { data } = response;
-    //     const { id, name, occupation, weapon, cartoon } = data;
-    //     console.log(id, name, occupation)
-
-    //     const newCharacterHTML = `
-    //         <div class="character-info" id ="character-info">
-    //             <div class =${id}>ID: ${id}</div>
-    //             <div class =${name}>Name: ${name}</div>
-    //             <div class =${occupation}>: Occupation: ${occupation}</div>
-    //             <div class =${weapon}>Waepon: ${weapon}</div>
-    //             <div class =${cartoon}>Cartoon: ${cartoon}</div>
-    //           </div>
-    //         `;
-    //     document.getElementById('character-info').remove();
-    //     document.getElementById('characters-container').innerHTML += newCharacterHTML;
-    //   })
   }
 
   createOneRegister() {
-
+    const name = document.getElementById('name').value;
+    const occupation = document.getElementById('occupation').value;
+    const weapon = document.getElementById('weapon').value;
+    const cartoon = document.getElementById('cartoon').value;
+    console.log(axios.post(`${this.BASE_URL}/characters`, {name, occupation, weapon, cartoon }))
+    axios.post(`${this.BASE_URL}/characters`, {name, occupation, weapon, cartoon })
   }
 
   updateOneRegister() {
-
+    const id = document.getElementById('id-upd').value;
+    const name = document.getElementById('name-upd').value;
+    const occupation = document.getElementById('occupation-upd').value;
+    const weapon = document.getElementById('weapon-upd').value;
+    const cartoon = document.getElementById('cartoon-upd').value;
+    axios.put(`${this.BASE_URL}/characters/${id}`, {name, occupation, weapon, cartoon })
   }
 
   deleteOneRegister() {
-
+    document.querySelector('.characters-container').innerHTML = "";
+    axios.delete(`${this.BASE_URL}/characters/${document.getElementById("delete-one-input").value}`)
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,11 +1,12 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready(() => {
-  document.getElementById('fetch-all').onclick = function () {
+  document.getElementById('fetch-all').onclick = event => {
+    event.preventDefault();
     charactersAPI.getFullList().then(data => {
       data.forEach(character => {
         const { id, name, occupation, weapon, cartoon } = character;
-        // console.log(id, name, occupation, weapon, cartoon)
+        console.log(id, name, occupation, weapon, cartoon)
         const newCharacterHTML = `
             <div class="character-info" id="character-info">
               <div class =${id}>ID: ${id}</div>
@@ -20,7 +21,8 @@ $(document).ready(() => {
     })
   }
 
-  document.getElementById('fetch-one').onclick = function () {
+  document.getElementById('fetch-one').onclick = event => {
+    event.preventDefault();
     charactersAPI.getOneRegister().then(data => {
       const { id, name, occupation, weapon, cartoon } = data;
       console.log(id, name, occupation, weapon, cartoon)
@@ -37,15 +39,19 @@ $(document).ready(() => {
     })
   }
 
-  document.getElementById('delete-one').onclick = function () {
-
+  document.getElementById('delete-one').onclick = event => {
+    event.preventDefault();
+    charactersAPI.deleteOneRegister()
   }
 
-  document.getElementById('edit-character-form').onsubmit = function () {
-
+  document.getElementById('edit-character-form').onsubmit = function() { 
+    event.preventDefault();
+    charactersAPI.updateOneRegister()
   }
 
-  document.getElementById('new-character-form').onsubmit = function () {
-
+  document.getElementById('new-character-form').onsubmit = event => {
+    event.preventDefault();
+    console.log("creatd one")
+    charactersAPI.createOneRegister()
   }
 })
